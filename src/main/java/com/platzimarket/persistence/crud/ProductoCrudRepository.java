ab1241ee8b6d708439f66f667c86900ccbf68ee6
developerleonardo <leo.11hnz@gmail.com>
2023-12-02 17:40:23 -0500

    Query Methods

--- src/main/java/com/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,11 @@ package com.platzimarket.persistence.crud;
 import com.platzimarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> FindByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
